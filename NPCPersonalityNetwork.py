bc1295b2653e967810b63b0be50e2a8e4f90b38e
John Robison <10904512@live.mercer.edu>
2016-04-03 01:20:50 -0400

    Created the client that trains the network.

--- NPCPersonalityNetwork.py
+++ NPCPersonalityNetwork.py
@@ -26,7 +26,7 @@ numOutputs = 6
 
 # friend, shop, importance, aggressive, inCombat,
 #  [attack, trade, ignore, run, quest, aid]
-training_set = [
+training_sets = [
     [0.0, 0.0, 0.0, 0.0, 0.0, [0, 0, 1, 0, 0, 0]],
     [1.0, 1.0, 0.0, 0.0, 0.0, [0, 1, 0, 0, 0, 0]],
     [-1.0, 0.0, 0.0, 1.0, 0.0, [1, 0, 0, 0, 0, 0]],
@@ -41,25 +41,26 @@ training_set = [
 output_strings = ["Attack", "Trade", "Ignore", "Run",
                   "Give Quest", "Aid Player"]
 
-input_to_hidden = [[0.0 for x in range(numHiddens)] for y in range(numInputs)]
+input_to_hidden = [[0.0 for x in range(numHiddens)]
+                   for y in range(numInputs + 1)]
 hidden_to_output = [[0.0 for x in range(numOutputs)]
-                    for y in range(numHiddens)]
+                    for y in range(numHiddens + 1)]
 
-inputs = []
-hidden = []
-target = []
-actual = []
+inputs = [0.0 for x in range(numInputs)]
+hiddens = [0.0 for x in range(numHiddens)]
+target = [0.0 for x in range(numOutputs)]
+actual = [0.0 for x in range(numOutputs)]
 
-hidden_error = []
-output_error = []
+hidden_error = [0.0 for x in range(numHiddens)]
+output_error = [0.0 for x in range(numOutputs)]
 
 learning_rate = 0.2
-number_of_epochs = 10000
+number_of_epochs = 100000
 
 
 # Creates a random number between -0.5 and 0.5
 def randomWeight():
-    return random.random() - 0.5
+    return random() - 0.5
 
 
 # Randomly initializes the weights of the network
@@ -92,14 +93,14 @@ def feedForward():
 
         sum += input_to_hidden[numInputs][hidden]
 
-        hidden[hidden] = sigmoid(sum)
+        hiddens[hidden] = sigmoid(sum)
 
     # Computes the output of the network
     for output in range(numOutputs):
         sum = 0.0
 
         for hidden in range(numHiddens):
-            sum += hidden[hidden] * hidden_to_output[hidden][output]
+            sum += hiddens[hidden] * hidden_to_output[hidden][output]
 
         sum += hidden_to_output[numHiddens][output]
 
@@ -121,14 +122,14 @@ def backPropagate():
             hidden_error[hidden] += (output_error[output] *
                                      hidden_to_output[hidden][output])
 
-        hidden_error[hidden] *= sigmoidDerivative(hidden[hidden])
+        hidden_error[hidden] *= sigmoidDerivative(hiddens[hidden])
 
     # Updates the weights of the hidden layer based on the errors
     for output in range(numOutputs):
         for hidden in range(numHiddens):
             hidden_to_output[hidden][output] += (learning_rate *
                                                  output_error[output] *
-                                                 hidden[hidden])
+                                                 hiddens[hidden])
 
         hidden_to_output[numHiddens][output] += (learning_rate *
                                                  output_error[output])
@@ -143,4 +144,47 @@ def backPropagate():
         input_to_hidden[numInputs][hidden] += (learning_rate *
                                                hidden_error[hidden])
 
+
+def action():
+    index = 0
+
+    maximum = actual[index]
+
+    for i in range(numOutputs):
+        if(actual[i] > maximum):
+            maximum = actual[i]
+            index = i
+
+    return index
+
 statistic_file = open("statistics.txt", "w")
+
+assignRandomWeights()
+
+sample = 0
+max_samples = len(training_sets) - 1
+
+for i in range(number_of_epochs + 1):
+    inputs = training_sets[sample][0:5]
+    target = training_sets[sample][5]
+
+    feedForward()
+
+    error = 0.0
+    for j in range(numOutputs):
+        error += (training_sets[sample][5][j] - actual[j]) ** 2
+
+    error = 0.5 * error
+
+    if i % 50 == 0:
+        statistic_file.write(str(i) + ", " + str(error) + "\n")
+        print(str(i), ", " + str(error))
+
+    backPropagate()
+
+    if sample == max_samples:
+        sample = 0
+    else:
+        sample += 1
+
+statistic_file.close()
